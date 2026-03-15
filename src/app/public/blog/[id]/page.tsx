6c6fe1cd3ccd81e3e1443ba42bd3103b755dd08b
AhmedMo100 <am01017493939@gmail.com>
2026-03-15 07:55:17 +0200

    Remove blog post API route since BlogPostPage no longer exists

--- src/app/public/blog/[id]/page.tsx
+++
@@ -1,8 +0,0 @@
-import BlogPostPage from '@/components/public/blog/BlogPostPage'
-
-type Props = { params: Promise<{ id: string }> }
-
-export default async function Page({ params }: Props) {
-  const { id } = await params
-  return <BlogPostPage id={Number(id)} />
-}
